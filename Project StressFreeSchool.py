dc2a09d44ed2f1112ab400e901e1befb37312b94
13623 <13623@burnside.school.nz>
2016-05-28 22:58:14 +1200

    More progress, folder organised
    Some more work on the program, added another function and some more.
    About 60% finished in terms of functionality

--- Project StressFreeSchool.py
+++ Project StressFreeSchool.py
@@ -5,15 +5,27 @@ import os.path
 def main():
     global name
     global profession
+    global yourTest
     name = input("What is your first name?")
-    name = name + input('What is your last name?')+'.txt'  # Name of text file coerced with +.txt
+    name = name + " " + input('What is your last name?')
     profession = input("Are you a student or a teacher?")
-    test()
+    try:
+        yourTest = open(os.path.join(os.getcwd()+"\Database",name +'.txt' ),'r+')
+        print("We see that you have already done this test. Would you like to try again or just view your previous results?")
+        print("By agreeing to try again, you will lose your previous results.")
+        againInput = input("Press a to try again and anything else to view your results")
+        if againInput == "a":
+            yourTest.truncate()
+            test()
+        else:
+            review(yourTest)
+    except FileNotFoundError:
+        yourTest = open(os.path.join(os.getcwd()+"\Database",name +'.txt'),'w') #initialising the text file to record responses
+        test()
 
 def test():
-    yourTest = open(name,'w') #initialising the text file to record responses
     responses = {} #creating a dictionary to record responses
-    Questions = open(profession+".txt",'r') #opening the text file to read questions from
+    Questions = open(os.path.join(os.getcwd()+"\Resources",profession+".txt"),'r') #opening the text file to read questions from
     QuestionNo = 0 #initialising the QuestionNo variable
     TotalScore = 0 #initialising the TotalScore variable
     for line in Questions:
@@ -31,9 +43,46 @@ def test():
                 print("You didn't type in a number. Please try again")
         yourTest.write (line)
         yourTest.write (str(answer))
-        
         responses[QuestionNo] = answer
         TotalScore = TotalScore + answer
+        print (responses)
     print (TotalScore)
-        
+    results(TotalScore)
+
+def results(TotalScore):
+    print ("Hi {0}!".format(name))
+    print ("You said you are a {0}, and now the results are here!".format(profession))
+    if profession == "students":
+        if TotalScore < 99:
+            status = "normal"
+        elif TotalScore > 99 and TotalScore < 131:
+            status = "low"
+        elif TotalScore > 131 and TotalScore < 177:
+            status = "moderate"
+        else:
+            status = "high"
+    elif profession == "teachers":
+        print("working")
+        if TotalScore < 60:
+            print("working")
+            status = "normal"
+        elif TotalScore > 61 and TotalScore < 81:
+            print("working")
+            status = "low"
+        elif TotalScore > 82 and TotalScore < 110:
+            print("working")
+            status = "moderate"
+        else:
+            print("working")
+            status = "high"
+    advice = open(os.path.join(os.getcwd()+"\Resources",status+'.txt'),'r')
+    for line in advice:
+        print (line)
+        yourTest.write(line)
+    yourTest.close()
+    print("Going Back to main menu")
+    
+            
+            
+    
 main()
