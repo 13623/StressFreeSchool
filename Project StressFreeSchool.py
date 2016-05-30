0b934c87295216113c14b3def7b6e2b21c81c2cc
13623 <13623@burnside.school.nz>
2016-05-31 09:20:10 +1200

    31/05 Some more progress, added time module, more robust

--- Project StressFreeSchool.py
+++ Project StressFreeSchool.py
@@ -2,6 +2,7 @@ import webbrowser
 import audioop
 import tkinter
 import os.path
+import time
 def main():
     global name
     global profession
@@ -10,22 +11,33 @@ def main():
     name = name + " " + input('What is your last name?')
     profession = input("Are you a student or a teacher?")
     try:
-        yourTest = open(os.path.join(os.getcwd()+"\Database",name +'.txt' ),'r+')
+        yourTest = open(os.path.join(os.getcwd()+"\Database",name +'.txt' ),'r+') #If the person has already done the test and the file is in the database, it will be able to open
         print("We see that you have already done this test. Would you like to try again or just view your previous results?")
         print("By agreeing to try again, you will lose your previous results.")
         againInput = input("Press a to try again and anything else to view your results")
         if againInput == "a":
-            yourTest.truncate()
+            yourTest.truncate() #Wipe everything on the text file, to prevent writing things over it
             test()
         else:
             review(yourTest)
-    except FileNotFoundError:
+    except FileNotFoundError: #This means the user hasn't done the test yet therefore no file for him in the database
         yourTest = open(os.path.join(os.getcwd()+"\Database",name +'.txt'),'w') #initialising the text file to record responses
         test()
 
 def test():
     responses = {} #creating a dictionary to record responses
-    Questions = open(os.path.join(os.getcwd()+"\Resources",profession+".txt"),'r') #opening the text file to read questions from
+    while True:
+        try:
+            Questions = open(os.path.join(os.getcwd()+"\Resources",profession+".txt"),'r') #opening the text file to read questions from
+        except FileNotFoundError:
+            if profession[1] == "s" or profession[1] == "S":
+                print("I take it as you meant student")
+                profession = "students"
+            elif profession[1] == "t" or profession[1] == "t":
+                print("I take it as you meant student")
+                profession = "teachers"
+            else:
+                profession = input("I have no idea what you typed in. Please try again")
     QuestionNo = 0 #initialising the QuestionNo variable
     TotalScore = 0 #initialising the TotalScore variable
     for line in Questions:
@@ -45,7 +57,6 @@ def test():
         yourTest.write (str(answer))
         responses[QuestionNo] = answer
         TotalScore = TotalScore + answer
-        print (responses)
     print (TotalScore)
     results(TotalScore)
 
@@ -62,23 +73,19 @@ def results(TotalScore):
         else:
             status = "high"
     elif profession == "teachers":
-        print("working")
         if TotalScore < 60:
-            print("working")
             status = "normal"
         elif TotalScore > 61 and TotalScore < 81:
-            print("working")
             status = "low"
         elif TotalScore > 82 and TotalScore < 110:
-            print("working")
             status = "moderate"
         else:
-            print("working")
             status = "high"
     advice = open(os.path.join(os.getcwd()+"\Resources",status+'.txt'),'r')
     for line in advice:
         print (line)
         yourTest.write(line)
+        time.sleep(len(line)/8) #wait total characters/8 seconds to give the user a bit of time to read before it prints the next line
     yourTest.close()
     print("Going Back to main menu")
     
