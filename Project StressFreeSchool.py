15f677c7c57a0df6d2ca60e937ddd02f0473342d
13623 <13623@burnside.school.nz>
2016-06-07 14:43:43 +1200

    Complete exceptional data testing

--- Project StressFreeSchool.py
+++ Project StressFreeSchool.py
@@ -9,18 +9,24 @@ def main():
     global yourTest
     name = input("What is your first name?")    
     name = name + " " + input('What is your last name?')
-    if any(char.isdigit() for char in name) == True:
-        print("That wasn't your name. Please try again")
+    if any(char.isdigit() for char in name) == True: #Check if the user typed in a number
+        print("That is not your name. Your name doesn't have numbers. Please try again")
+        main()
+    elif set('[~!@#$%^&*()_+{}":;\']+$').intersection(name): # checks if the user typed in a special character
+        print ("You put a special character. That's not your name.")
+        main()
+    elif name[0] == " " or '  '.join(name.split()) == name.strip(): #Check if the user typed in nothing but space
+        print ("It looks wrong. Check if you put space in your name or anything at all.")
         main()
     profession = input("Are you a student or a teacher?")
-    if profession[0] == "s" or profession[1] == "S":
+    if profession[0] == "s" or profession[0] == "S":
         print("I take it as you meant student")
         profession = "students"
-    elif profession[0] == "t" or profession[1] == "t":
+    elif profession[0] == "t" or profession[0] == "T":
         print("I take it as you meant teacher")
         profession = "teachers"
     else:
-        profession = input("I have no idea what you typed in. Please try again")
+        profession = input("I have no idea what you typed in. Please try again: ")
         main()
     try:
         yourTest = open(os.path.join(os.getcwd()+"\Database",name +'.txt' ),'r+') #If the person has already done the test and the file is in the database, it will be able to open
@@ -53,12 +59,7 @@ def test():
                 else:
                     print("You can only type in an integer between 1 ~ 5.")
             except ValueError:
-                print("You didn't type in a number. Please try again")
-            try:
-                if len(answer) > 0:
-                    break
-            except TypeError:
-                print("You didn't type in anything. Please try again")            
+                print("You didn't type in a number. Please try again")     
         yourTest.write (line)
         yourTest.write (str(answer))
         responses[QuestionNo] = answer
